c2f8d4b359bef328fadeff04322864b5f93abeff
vidya <vidyagounder29@gmail.com>
2025-02-25 12:28:24 +0530

    abc

--- Github/src/Main.java
+++ Github/src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
-public static void main(String[] args) {
-	System.out.println("Vidya");
-	System.out.println("Vidya gounder");
-}
+	public static void main(String[] args) {
+		System.out.println("Vidya");
+		System.out.println("Vidya gounder");
+	}
 }
